0cf57f8fa196d0a4c62c2300a27484e67b32dfb5
SmilesDev <smilesiosteam@gmail.com>
2023-10-02 19:33:51 +0500

    fix: Added package os support

--- Package.swift
+++ Package.swift
@@ -4,7 +4,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "SmilesSubscriptionPromotion",
+    name: "SmilesSubscriptionPromotion",platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
